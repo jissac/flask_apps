97def33466eca1ca054b1df21ede523c40107ee9
James I <jissac@users.noreply.github.com>
2020-03-17 23:45:05 -0400

    updated submit form

--- flask_crud/intro_form/app.py
+++ flask_crud/intro_form/app.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template,url_for,redirect, request
+from flask import Flask,render_template,url_for,redirect,request, session,flash
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 import yaml
+import os
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -14,6 +16,7 @@ app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -30,12 +33,17 @@ def index():
     #         users = cur.fetchall()
     #         print(users)
     if request.method == 'POST':
-        form = request.form
-        name = form['name']
-        age = form['age']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO employee(name,age) VALUES(%s,%s)",(name,age))
-        mysql.connection.commit()
+        try:
+            form = request.form
+            name = form['name']
+            age = form['age']
+            cur = mysql.connection.cursor()
+            name = generate_password_hash(name)
+            cur.execute("INSERT INTO employee(name,age) VALUES(%s,%s)",(name,age))
+            mysql.connection.commit()
+            flash('Successfully inserted data!','success')
+        except:
+            flash('Failed to insert data','danger')
     return render_template('index.html')
 
 @app.route('/employees')
@@ -44,6 +52,7 @@ def employees():
     result_value = cur.execute("SELECT * FROM employee")
     if result_value > 0:
         employees = cur.fetchall()
+        # session['username'] = employees[0]['name']
         return render_template('employees.html',employees=employees)
 
 @app.route('/about')
